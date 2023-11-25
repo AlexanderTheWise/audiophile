42e1315f8e58c1d0b8e23fcab8bbce70e1203adf
AlexanderTheWise <aguillenhernandez@hotmail.com>
2023-11-25 19:35:42 +0100

    Add SlideNav

--- components/Icons/ArrowRight.tsx
+++ components/Icons/ArrowRight.tsx
@@ -1,11 +1,16 @@
 const ArrowRight = () => (
-  <svg width="8" height="12" xmlns="http://www.w3.org/2000/svg">
+  <svg
+    focusable="false"
+    width="8"
+    height="12"
+    xmlns="http://www.w3.org/2000/svg"
+  >
     <path
       d="M1.322 1l5 5-5 5"
       stroke="#D87D4A"
-      stroke-width="2"
+      strokeWidth="2"
       fill="none"
-      fill-rule="evenodd"
+      fillRule="evenodd"
     />
   </svg>
 );
--- components/Navigation/Links/CategoryLink.tsx
+++ components/Navigation/Links/CategoryLink.tsx
@@ -1,12 +1,24 @@
 import Link from "next/link";
 import ArrowRight from "../../Icons/ArrowRight";
 
-const CategoryLink = ({ href }: { href?: string }) => (
-  <Link href="" role="menuitem" className="flex btn--4 category-card__link">
-    <div className="flex category-card__link-content">
-      shop <ArrowRight />
-    </div>
-  </Link>
-);
+type CategoryLinkProps = {
+  href: string;
+  label: string;
+};
+
+const CategoryLink = ({ href, label }: CategoryLinkProps) => {
+  return (
+    <Link
+      id="category_card_link"
+      href={href}
+      aria-label={label}
+      className="flex btn--4 category-card__link"
+    >
+      <div className="flex category-card__link-content">
+        shop <ArrowRight />
+      </div>
+    </Link>
+  );
+};
 
 export default CategoryLink;
