d56e7a7431c30a02ab5db2341fe968457ba79003
AlexanderTheWise <aguillenhernandez@hotmail.com>
2023-11-27 21:54:37 +0100

    Add social media icons

---
+++ components/Icons/index.ts
@@ -0,0 +1,5 @@
+export { default as ArrowRight } from "./ArrowRight";
+export { default as FaceBook } from "./FaceBook";
+export { default as Instagram } from "./Instagram";
+export { default as Logo } from "./Logo";
+export { default as Twitter } from "./Twitter";
