808e2920059b975ce3cc22a9d8da3471dd00c2be
AlexanderTheWise <aguillenhernandez@hotmail.com>
2023-12-16 17:12:08 +0100

    Add product cart context provider

---
+++ app/_context/ProductCart/index.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { createContext, useReducer, Dispatch, useMemo } from "react";
+import { productCartReducer } from "@/app/store/ProductCart/reducer";
+import {
+  ProductCart,
+  UnionProductActions,
+} from "@/app/store/ProductCart/types";
+
+type ProductCartContext = {
+  state: ProductCart;
+  dispatch: Dispatch<UnionProductActions>;
+};
+
+type ProductCartProviderProps = {
+  children: React.ReactNode;
+  initalState: ProductCart;
+};
+
+export const ProductCartContext = createContext<ProductCartContext>({
+  state: [],
+  dispatch: () => {},
+});
+
+const ProductCartProvider = ({
+  children,
+  initalState,
+}: ProductCartProviderProps) => {
+  const [state, dispatch] = useReducer(productCartReducer, initalState);
+
+  const value = useMemo(() => ({ state, dispatch }), [state, dispatch]);
+
+  return (
+    <ProductCartContext.Provider value={value}>
+      {children}
+    </ProductCartContext.Provider>
+  );
+};
+
+export default ProductCartProvider;
