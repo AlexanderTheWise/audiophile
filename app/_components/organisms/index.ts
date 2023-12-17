ccc3f0d69bec82ce1a5098669394ba858916b0c0
AlexanderTheWise <aguillenhernandez@hotmail.com>
2023-12-17 17:47:39 +0100

    Add YouMayAlsoLike section

--- app/_components/organisms/index.ts
+++ app/_components/organisms/index.ts
@@ -5,3 +5,7 @@ export { default as HomeProducts } from "./HomeProducts";
 export { default as BestAudioGear } from "./BestAudioGear";
 export { default as CategoryProduct } from "./CategoryProduct";
 export { default as ProductDetail } from "./ProductDetail";
+export { default as Features } from "./Features";
+export { default as InTheBox } from "./InTheBox";
+export { default as Gallery } from "./Gallery";
+export { default as YouMayAlsoLike } from "./YouMayAlsoLike";
