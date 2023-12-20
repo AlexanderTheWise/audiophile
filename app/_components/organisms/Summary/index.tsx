78cb91c6a4ec46714d6f2a0c17b126b6c58eaf8c
AlexanderTheWise <aguillenhernandez@hotmail.com>
2023-12-20 01:29:13 +0100

    Add Summary

---
+++ app/_components/organisms/Summary/index.tsx
@@ -0,0 +1,67 @@
+"use client";
+
+import { useContext } from "react";
+import { ButtonPrimary, ParBTrans, WhiteBox } from "@/_components/atoms";
+import { ProductCartContext } from "@/_context/ProductCart";
+import { ProductTile } from "@/_components/molecules";
+import { getTotalPrice, getVat } from "@/lib/products";
+import "./summary.scss";
+
+const shipping = 50;
+
+const Summary = () => {
+  const { state } = useContext(ProductCartContext);
+
+  const totalPrice = getTotalPrice(state);
+  const vat = getVat(totalPrice);
+
+  return (
+    <WhiteBox className="summary flex-col row-gap-32px">
+      <h2 className="text--uppercase">summary</h2>
+
+      <ul className="flex-col row-gap-24px">
+        {Object.entries(state).map(([id, productTile]) => (
+          <li key={id}>
+            <ProductTile productTile={productTile}>
+              <ParBTrans className="text--bold">x{productTile.count}</ParBTrans>
+            </ProductTile>
+          </li>
+        ))}
+      </ul>
+
+      <div className="flex-col row-gap-8px">
+        <div className="flex items-center content-space-between">
+          <ParBTrans className="text--uppercase">total</ParBTrans>
+          <p className="text--bold">$ {totalPrice.toLocaleString("en-US")}</p>
+        </div>
+        <div className="flex items-center content-space-between">
+          <ParBTrans className="text--uppercase">shipping</ParBTrans>
+          <p className="text--bold">$ {shipping.toLocaleString("en-US")}</p>
+        </div>
+        <div className="flex items-center content-space-between">
+          <ParBTrans className="text--uppercase">vat (included)</ParBTrans>
+          <p className="text--bold">$ {vat.toLocaleString("en-US")}</p>
+        </div>
+        <div className="summary__grand-total flex items-center content-space-between">
+          <ParBTrans className="text--medium text--uppercase">
+            grand total
+          </ParBTrans>
+          <p className="text--bold text--brand">
+            $ {(shipping + totalPrice).toLocaleString("en-US")}
+          </p>
+        </div>
+      </div>
+
+      <ButtonPrimary
+        className="summary__submit-button"
+        onClick={() => {}}
+        type="submit"
+        form="checkout-form"
+      >
+        continue & pay
+      </ButtonPrimary>
+    </WhiteBox>
+  );
+};
+
+export default Summary;
---
+++ app/_components/organisms/Summary/test.tsx
@@ -0,0 +1,97 @@
+import renderWithProviders from "@/_testUtils/renderWithProviders";
+import { ProductCart } from "@/store/ProductCart/types";
+import { getTotalPrice, getVat } from "@/lib/products";
+import Summary from ".";
+
+const initialState: ProductCart = {
+  [1]: {
+    name: "YX1 Wireless Earphones",
+    cartImage: "/images/cart/yx1.jpg",
+    count: 1,
+    price: 200,
+  },
+  [2]: {
+    name: "XX59 Headphones",
+    cartImage: "/images/cart/xx59.jpg",
+    count: 2,
+    price: 300,
+  },
+};
+
+const renderSummary = () => renderWithProviders(<Summary />, { initialState });
+
+describe("<Summary /> component", () => {
+  const totalPrice = getTotalPrice(initialState);
+  const vat = getVat(totalPrice);
+  const shipping = 50;
+
+  describe("when there are two products in the product cart", () => {
+    it("should show a level 2 heading called 'Summary'", () => {
+      const level = 2;
+      const name = "summary";
+      const { getByRole } = renderSummary();
+
+      const heading = getByRole("heading", { level, name });
+
+      expect(heading).toBeInTheDocument();
+    });
+
+    it("should show two images with the names of the products", () => {
+      const names = [initialState[1].name, initialState[2].name];
+      const { getByRole } = renderSummary();
+
+      names.forEach((name) => {
+        const image = getByRole("img", { name });
+
+        expect(image).toBeInTheDocument();
+      });
+    });
+
+    it("should show two texts with the names of the products without their categories", () => {
+      const ids = ["YX1 Wireless", "XX59"];
+      const { getByText } = renderSummary();
+
+      ids.forEach((id) => {
+        const text = getByText(id);
+
+        expect(text).toBeInTheDocument();
+      });
+    });
+
+    it("should show a text with the total price of the products", () => {
+      const id = `$ ${totalPrice.toLocaleString("en-US")}`;
+      const { getByText } = renderSummary();
+
+      const text = getByText(id);
+
+      expect(text).toBeInTheDocument();
+    });
+
+    it("should show a text with the shipping price", () => {
+      const id = `$ ${shipping.toLocaleString("en-US")}`;
+      const { getByText } = renderSummary();
+
+      const text = getByText(id);
+
+      expect(text).toBeInTheDocument();
+    });
+
+    it("should show a text with the vat", () => {
+      const id = `$ ${vat.toLocaleString("en-US")}`;
+      const { getByText } = renderSummary();
+
+      const text = getByText(id);
+
+      expect(text).toBeInTheDocument();
+    });
+
+    it("should show a text with the shipping price plus the total price", () => {
+      const id = `$ ${(totalPrice + shipping).toLocaleString("en-US")}`;
+      const { getByText } = renderSummary();
+
+      const text = getByText(id);
+
+      expect(text).toBeInTheDocument();
+    });
+  });
+});
