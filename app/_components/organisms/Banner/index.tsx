3a5705c06073266511bdcade82cf3681033e7c70
AlexanderTheWise <aguillenhernandez@hotmail.com>
2023-12-22 00:54:54 +0100

    Delete SliderNav component

--- app/_components/organisms/Banner/index.tsx
+++ app/_components/organisms/Banner/index.tsx
@@ -1,17 +1,29 @@
-import { Audiophile } from "@/_components/atoms";
-import { CommonProps } from "@/_components/types";
+import { useContext } from "react";
+import { Audiophile, Burger, CartButton } from "@/_components/atoms";
 import { PagesLinks } from "@/_components/molecules";
-import SliderNav from "../SliderNav";
+import { CommonProps } from "@/_components/types";
+import { ModalContext } from "@/_context/Modal";
 import "./banner.scss";
 
-const Banner = ({ className = "" }: CommonProps) => (
-  <div className={`${className} banner flex items-center`}>
-    <div className="banner__content flex items-center content-space-between">
-      <SliderNav />
-      <Audiophile />
-      <PagesLinks className="banner__pages-links" />
+const Banner = ({ className = "" }: CommonProps) => {
+  const {
+    toggleModal,
+    modal: { categories, cart },
+  } = useContext(ModalContext);
+
+  const toggleBurgerModal = () => toggleModal("categories");
+  const toggleCartModal = () => toggleModal("cart");
+
+  return (
+    <div className={`${className} banner flex items-center`}>
+      <div className="banner__content flex items-center content-space-between">
+        <Burger onClick={toggleBurgerModal} isOpen={categories} />
+        <Audiophile />
+        <PagesLinks className="banner__pages-links" />
+        <CartButton onClick={toggleCartModal} isOpen={cart} />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Banner;
